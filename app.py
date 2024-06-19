3bbfc2cf9a03b8a68265aef164ed2ff601391c58
MauroRaya <maurorayabr@gmail.com>
2024-06-19 17:59:28 -0300

    interface melhorada com css

--- app.py
+++ app.py
@@ -10,6 +10,22 @@ COPA_ANO_FIM = 2015
 partidas = []
 
 
+def validar_ano(_ano_copa):
+    try:
+        _ano_copa = int(_ano_copa)
+
+    except ValueError:
+        return False
+    
+    except Exception:
+        return False
+    
+    if not (COPA_ANO_INICIO <= _ano_copa <= COPA_ANO_FIM):
+        return False
+    
+    return True
+
+
 @app.route('/')
 def index():
     return render_template('index.html', partidas=partidas)
@@ -19,7 +35,10 @@ def index():
 def scrape():
     global partidas
     ano_copa = request.form.get("ano_copa", None)
-    partidas = buscar_partidas(ano_copa)
+    
+    if validar_ano(ano_copa):
+        partidas = buscar_partidas(ano_copa)
+        
     return render_template('index.html', partidas=partidas)
 
 
--- static/main.js
+++ static/main.js
@@ -1,8 +1,9 @@
 async function editarPartida(elemento) {
-    let novoTexto = prompt('Editar partida: ');
+    let span      = elemento.parentElement.querySelector('span');
+    let texto     = span.textContent; 
+    let novoTexto = prompt('Editar partida: ', texto);
 
     if (novoTexto !== null && novoTexto.trim() !== "") {
-        let span = elemento.parentElement.querySelector('span');
         let listaNaoOrdenada = elemento.closest('ul');
         let liElement = elemento.closest('li');
         let indexPartida = Array.from(listaNaoOrdenada.children).indexOf(liElement);
